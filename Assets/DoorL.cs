3f39e00dd881802908fe0e30cc27e24a6b27d9e6
davidaguilar1 <100818462+davidaguilar1@users.noreply.github.com>
2025-10-06 15:20:35 -0400

    lights added

--- Assets/DoorL.cs
+++ Assets/DoorL.cs
@@ -3,14 +3,26 @@ using System;
 using UnityEngine.SceneManagement;
 public class DoorL : MonoBehaviour
 {
+    public GameObject Light;
     public void OnMouseDown()
     {
         SceneManager.LoadScene("Death");
     }
+    public void OnMouseOver()
+    {
+        Light.SetActive(true);
+
+
+
+    }
+    public void OnMouseExit()
+    {
+        Light.SetActive(false);
+    }
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        Light.SetActive(false);
     }
 
     // Update is called once per frame
